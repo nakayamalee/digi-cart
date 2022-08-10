e8ccb1ede6e2f0457a47d036c053ef98595b8c1b
nakayamalee <93628098+nakayamalee@users.noreply.github.com>
2022-08-10 11:28:45 +0800

    softDelete

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     protected $table = 'products';
     protected $primaryKey = 'id';
-    protected $fillable = ['created_at', 'updated_at','img_path','product_name','product_qty','product_price'];
+    protected $fillable = ['created_at', 'updated_at','img_path','product_name','product_qty','product_price','deleted_at'];
 }
